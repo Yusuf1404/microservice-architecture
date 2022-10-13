dfe35add49db43ea47a7b0bfbe7b41f5be0dfeb9
Yusuf1404 <yusufyucel1404@hotmail.com>
2022-10-13 11:24:07 +0200

    hej

---
+++ ProductMicroservice/Data/DBContextClass,cs.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductMicroservice.Model;
+
+namespace ProductMicroservice.Data
+{
+    public class DBContextClass_cs: DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DBContextClass_cs(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ UserMicroservice/Data/DBContextClass,cs.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserMicroservice.Model;
+
+namespace UserMicroservice.Data
+{
+    public class DBContextClass_cs: DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DBContextClass_cs(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
